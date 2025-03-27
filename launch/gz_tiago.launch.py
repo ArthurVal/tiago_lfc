a1292ab64bfc8e9d030cbba70263411b496e978c
Arthur Valiente <avaliente@laas.fr>
2025-03-27 17:16:28 +0100

    Minor renaming / adding docs

--- launch/gz_tiago.launch.py
+++ launch/gz_tiago.launch.py
@@ -26,14 +26,23 @@ from tiago_sim.launch import (
 
 def generate_launch_description():
     """Launch tiago within GZ."""
-    description, args_names = declare_arguments_from_yaml(
-        file_path=Path(
-            get_package_share_directory('tiago_description'),
-            'config',
-            'tiago_configuration.yaml',
-        ),
+    tiago_xacro_file = Path(
+        get_package_share_directory('tiago_description'),
+        'robots',
+        'tiago.urdf.xacro',
     )
 
+    tiago_xacro_mappgins_yaml_description = Path(
+        get_package_share_directory('tiago_description'),
+        'config',
+        'tiago_configuration.yaml',
+    )
+
+    description, xacro_mappings_argument_names = declare_arguments_from_yaml(
+        file_path=tiago_xacro_mappgins_yaml_description,
+    )
+
+    # Since we are simulating, use_sim_time is FORCED here
     description.add_action(
         SetLaunchConfiguration(
             'use_sim_time',
@@ -41,27 +50,28 @@ def generate_launch_description():
         ),
     )
 
-    # 'use_sim_time' it not included in the .yaml file
+    # 'use_sim_time' it not included in the yaml file but used inside the xacro
+    # as a mapping to select Gazebo stuff...
     # We add it by hand.
-    args_names.append('use_sim_time')
+    xacro_mappings_argument_names.append('use_sim_time')
 
-    # TODO: create a tmp file ?
-    tiago_urdf_file = Path(
-        get_package_share_directory('tiago_description'),
-        'robots',
-        'tiago.urdf',
-    )
+    # FIXME: We shouldn't create a file at this location...
+    tiago_urdf_file = tiago_xacro_file.with_suffix('.urdf')
+    # Some possible work around TBD:
+    # - Create a tmp file ?
+    # - Use a fifo (not portable in windows and I don't even know if it works
+    #   well with gz_spawn...) ?
+    # - Find out how to forward a valid value using 'sdf' param instead of
+    #   'sdf_filename' in gz service /world/create (EntityFactory msg) ?
+    # - ... ?
 
     add_robot_description_from_xacro(
-        file_path=Path(
-            get_package_share_directory('tiago_description'),
-            'robots',
-            'tiago.urdf.xacro',
-        ),
+        file_path=tiago_xacro_file,
         mappings={
             names: LaunchConfiguration(names)
-            for names in args_names
+            for names in xacro_mappings_argument_names
         },
+        # TODO: Remove this
         output_file=tiago_urdf_file,
         description=description,
     )
@@ -76,9 +86,15 @@ def generate_launch_description():
         description=description,
     )
 
-    # We reuse the world of gz_server declared as argument
-    # and update it (only use the stem) for gz_spawn_entity
+    # NOTE:
+    # gz_server declare 'world' as an argument, but require a '.sdf' file.
+    # Then, when calling GZ services associated to that world, the world is
+    # mentionned without its file extension.
+    # This is this 'world' (without the file extension) that is used by
+    # gz_spawn_entity below.
     world = LaunchConfiguration('world')
+
+    # This remove the file extension from 'world'
     description.add_action(
         Invoke(
             lambda v: Path(v).stem,
